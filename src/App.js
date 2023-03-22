ddc97ce1b41773420c2a71ecdbb39a98382c2e89
Felipe Gomes Pellegrini <107892258+FelipeGPellegrini@users.noreply.github.com>
2023-03-22 15:26:13 -0300

    Finish

--- src/App.js
+++ src/App.js
@@ -1,23 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [buttonSort, setButtonSort] = useState(true);
+
+  const [name, setName] = useState("");
+
+  const [nameSorted, setNameSorted] = useState("");
+
+  let [items, setItems] = useState([]);
+
+  const [sorted, setSorted] = useState(false);
+
+  function add() {
+    if (name !== "") {
+      items.push(name);
+      setName("");
+      setButtonSort(false);
+    }
+  }
+
+  function sort(e) {
+    e.preventDefault();
+    setNameSorted(items[Math.floor(Math.random() * items.length)]);
+    setSorted(true);
+  }
+
+  function remove() {
+    items.pop();
+    setItems([...items]);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <h1>Sorteador de nomes</h1>
+      <ul>
+        {items.map((item) => (
+          <div>
+            <li>{item}</li>
+          </div>
+        ))}
+      </ul>
+
+      <div className="containerInputs">
+        <input
+          type="text"
+          onChange={(e) => setName(e.target.value)}
+          value={name}
+          placeholder="Digite o nome..."
+        />
+        <div className="addName">
+          <button className="button" onClick={add}>
+            ADICIONAR
+          </button>
+          <button disabled={buttonSort} className="button" onClick={remove}>
+            REMOVER
+          </button>
+        </div>
+
+        <button
+          disabled={buttonSort}
+          id="sort"
+          className="button"
+          onClick={sort}
         >
-          Learn React
-        </a>
-      </header>
+          SORTEAR
+        </button>
+      </div>
+
+      <div className={sorted ? "sortedName" : "noSorted"}>
+        <h2>O nome sorteado foi:</h2>
+        <h3>{nameSorted}</h3>
+      </div>
     </div>
   );
 }
